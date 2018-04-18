263aeb92ecabb61f7c66462098afed98889ad500
Sohel Arian <ariantul@gmail.com>
2018-04-18 14:19:30 +0600

    commit and push

---
+++ app/src/main/java/com/example/wcs/celluloid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wcs.celluloid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/wcs/celluloid/MovieInfo.java
@@ -0,0 +1,13 @@
+package com.example.wcs.celluloid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_info);
+    }
+}
